127916f1c4d8d4c516ac795bd108f2530d7f8338
Rachel Xu <rachel960122@hotmail.com>
2016-01-23 17:13:49 -0500

    Generate autocorrect suggestions for dictionary entries and input entries

--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.ArrayList;
 import java.io.IOException;
 import java.io.FileWriter;
@@ -28,7 +29,39 @@ public class Autocorrect {
 				String[] twoWords = words.nextLine().split(" ");
 				map.put(twoWords[0], twoWords[1]);
 			}
-			
+
+
+			Scanner scanner = new Scanner(new FileInputStream("../data/dictionary.txt"));
+			ArrayList<String> dic = new ArrayList<String>();
+			while (scanner.hasNextLine()) {
+				dic.add(scanner.nextLine());
+			}
+
+			HashMap<String, ArrayList<String>> newDic = new HashMap<String, ArrayList<String>>();
+			for (String s : dic) {
+				HashSet<String> e1 = deleteChars(s);
+				HashSet<String> e2 = new HashSet<String>();
+				for (String str : e1) {
+					e2.addAll(deleteChars(str));
+				}
+				e1.addAll(e2);
+				for (String str : e1) {
+					ArrayList<String> list = newDic.get(str);
+					if (list != null) {
+						list.add(s);
+						newDic.put(str, list);
+					} else {
+						ArrayList<String> newList = new ArrayList<String>();
+						newList.add(s);
+						newDic.put(str, newList);
+					}
+				}
+			}
+			// for (String key : newDic.keySet()) {
+			// 	System.out.println(key + ": " + newDic.get(key));
+			// }
+			// System.out.println(newDic.size());
+
 			words = new Scanner(new FileInputStream("../data/input.txt"));
 			FileWriter writer = new FileWriter(new File("../data/output.txt"));
 			while (words.hasNextLine()) {
@@ -38,12 +71,18 @@ public class Autocorrect {
 						>= 0) {
 						writer.write(text[i] + " ");
 					} else {
-						if (i > 0) {
-							String correctWord = map.get(text[i-1].toLowerCase());
-							if (correctWord == null) correctWord = text[i];
-							writer.write(correctWord + " ");
-						} else {
-							writer.write(text[i] + " ");
+						HashSet<String> hs = deleteChars(text[i]);
+						Hashset<String> hs2 = new HashSet<String>();
+						for (String s : hs) {
+							hs2.addAll(deleteChars(s));
+						}
+						hs.addAll(hs2);
+						HashSet<String> options = new HashSet<String>();
+						for (String s : hs) {
+							ArrayList<String> res = newDic.get(s);
+							if (res != null) {
+								options.addAll(res);
+							}
 						}
 					}
 				}
@@ -53,8 +92,19 @@ public class Autocorrect {
  		}
  		catch (FileNotFoundException e) {
  			System.out.println("File not found");
- 		} catch (IOException e) {
-			e.printStackTrace();
+ 		} 
+ 	// 	catch (IOException e) {
+		// 	e.printStackTrace();
+		// }
+	}
+
+	private static HashSet<String> deleteChars(String s) {
+		HashSet<String> result = new HashSet<String>();
+		for (int i = 0; i < s.length(); i++) {
+			StringBuilder sb = new StringBuilder(s);
+			sb.deleteCharAt(i);	
+			result.add(sb.toString());
 		}
+		return result;
 	}
 }
