cfec6c24a59b1cbfe1c0b146a863466d0e367e23
Rachel Xu <rachel960122@hotmail.com>
2016-01-23 11:18:56 -0500

    Process 2gram data

--- src/DataProcess.java
+++ src/DataProcess.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -20,12 +21,12 @@ class WordFreq {
 public class DataProcess {
 	public static void main(String[] args) {
 		try {
-			Scanner two_grams = new Scanner(new File("../data/2gram.txt"));
+			Scanner two_grams = new Scanner(new FileInputStream("../data/2gram.txt"));
 			HashMap<String, WordFreq> map = new HashMap<String, WordFreq>();
 			
 			while (two_grams.hasNextLine()) {
 				String[] params = two_grams.nextLine().split("\t+");
-				System.out.println(Arrays.toString(params));
+				//System.out.println(Arrays.toString(params));
 				int freq = Integer.parseInt(params[0]);
 				String first = params[1], second = params[2];
 
@@ -38,13 +39,15 @@ public class DataProcess {
 				} else {
 					map.put(first, new WordFreq(second, freq));
 				}
+				
+			}
 
-				FileWriter writer = new FileWriter(new File("2gramOutput"));
+			FileWriter writer = new FileWriter(new File("2gramOutput.txt"));
 				for (String key : map.keySet()) {
 					writer.write(key + " " + map.get(key).word + "\n");
 				}
 				writer.close();
-			}
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
