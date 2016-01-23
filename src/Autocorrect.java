965304ace18dc2d0be41ccd5630332af406b88ed
Rachel Xu <rachel960122@hotmail.com>
2016-01-23 09:55:52 -0500

    Read the words in data file

---
+++ src/Autocorrect.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Autocorrect {
+	public static void main(String[] args) {
+		try {
+			Scanner words = new Scanner(new File("data/1gram.txt"));
+			ArrayList<String> wordList = new ArrayList<String>();
+			while (words.hasNextLine()) {
+				wordList.add(words.nextLine());
+			}
+			System.out.println(wordList.size());
+ 		}
+ 		catch (FileNotFoundException e) {
+ 			System.out.println("File not found");
+ 		}
+	}
+}
