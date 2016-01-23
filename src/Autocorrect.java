54b49a8394ad4e8f272539f7caa7ad8b34821bd2
Rachel Xu <rachel960122@hotmail.com>
2016-01-23 12:27:46 -0500

    Read 2gram data

--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -3,18 +3,26 @@
  */
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.ArrayList;
 
 public class Autocorrect {
 	public static void main(String[] args) {
 		try {
-			Scanner words = new Scanner(new File("../data/1gram.txt"));
+			Scanner words = new Scanner(new FileInputStream("../data/1gram.txt"));
 			ArrayList<String> wordList = new ArrayList<String>();
 			while (words.hasNextLine()) {
 				wordList.add(words.nextLine());
 			}
-			System.out.println(wordList.size());
+			words = new Scanner(new FileInputStream("../data//2gramOutput.txt"));
+			HashMap<String, String> map = new HashMap<String, String>();
+			while (words.hasNextLine()) {
+				String[] twoWords = words.nextLine().split(" ");
+				map.put(twoWords[0], twoWords[1]);
+			}
+			System.out.println(map.size());
  		}
  		catch (FileNotFoundException e) {
  			System.out.println("File not found");
